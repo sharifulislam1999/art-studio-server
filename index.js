23169d97da6a195b8032cd84274555bab974a1a9
Shariful Islam <shariful.feb1999@gmail.com>
2024-04-29 08:58:56 +0600

    init with semi complete

---
+++ index.js
@@ -0,0 +1,110 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const app = express();
+require('dotenv').config()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.guoefzb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db('b9a10');
+    const collection = database.collection("craftlist");
+    const categoryCollection = database.collection("category");
+    app.get("/",(req,res)=>{
+        res.send("server running")
+    });
+    app.get("/items",async(req,res)=>{
+        const cursor = collection.find();
+        const items = await cursor.toArray();
+        res.send(items);
+    });
+    app.get("/category",async(req,res)=>{
+      const cursor = categoryCollection.find();
+      const items = await cursor.toArray();
+      res.send(items)
+    })
+    app.get("/category/:name",async(req,res)=>{
+      const name = req.params.name;
+      const query = {category:name};
+      const items = collection.find(query);
+      const result = await items.toArray();
+      res.send(result)
+    })
+    app.get("/items/:useremail",async(req,res)=>{
+        const useremail = req.params.useremail;
+        const query = {useremail:useremail};
+        const items = collection.find(query);
+        const result = await items.toArray();
+        res.send(result);
+    })
+    app.get("/details/:id",async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:new ObjectId(id)};
+        const result = await collection.findOne(query);
+        res.send(result);
+    })
+    app.put("/update/:id",async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id:new ObjectId(id)};
+        const craft = req.body;
+        const updatedCraft = {
+          $set:{
+            itemName: craft.itemName,
+            category: craft.category,
+            price: craft.price,
+            rating: craft.rating,
+            customizeable: craft.customizeable,
+            time: craft.time,
+            photo: craft.photo,
+            description: craft.description,
+            stock: craft.stock
+          }
+        }
+        const result = await collection.updateOne(filter,updatedCraft)
+        res.send(result);
+    })
+    app.delete("/delete/:id",async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:new ObjectId(id)};
+        const result = await collection.deleteOne(query);
+        res.send(result);
+    })
+    app.get("/update/:id",async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:new ObjectId(id)};
+        const result = await collection.findOne(query);
+        res.send(result);
+    })
+    app.post('/additem',async(req,res)=>{
+        const data = req.body;
+        const result = await collection.insertOne(data);
+        res.send(result)   
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port,()=>{
+    console.log(`server runnig on port ${port}`)
+})
